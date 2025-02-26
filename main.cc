b993891504d03616680cebb5970d75984b0e708a
YiFanWei1 <>
2025-02-26 14:24:54 +0800

    1

--- main.cc
+++ main.cc
@@ -67,7 +67,7 @@ int main(int argc, char **argv)
 		 本例程只是为了演示如何将SDK中获取的图像，转成OpenCV的图像格式,以便调用OpenCV的图像处理函数进行后续开发
 	*/
 	CameraSetAeState(hCamera, false);
-	CameraSetExposureTime(hCamera, 10);
+	CameraSetExposureTime(hCamera, 1000);
 
 	if (tCapability.sIspCapacity.bMonoSensor)
 	{
@@ -80,7 +80,7 @@ int main(int argc, char **argv)
 		CameraSetIspOutFormat(hCamera, CAMERA_MEDIA_TYPE_BGR8);
 	}
 
-	const std::string model_path = "/home/wei/桌面/yolov8_pose-/model/yolov11_pose/best.xml";
+	const std::string model_path = "/home/wei/桌面/yolov8_pose-/model/best_openvino_model/best.xml";
 	// Define the confidence and NMS thresholds
 	const float confidence_threshold = 0.4;
 	const float NMS_threshold = 0.5;
